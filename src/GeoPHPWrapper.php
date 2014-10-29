acf606fa7942b216dd2cabd86b7fe1c6c92e2183
Pablo López <plopesc@gmail.com>
2014-10-29 15:29:59 +0100

    New implementation of GeoPHP service

--- src/GeoPHPWrapper.php
+++ src/GeoPHPWrapper.php
@@ -2,8 +2,25 @@
 
 namespace Drupal\geophp;
 
-class GeoPHPWrapper {
+class GeoPHPWrapper implements GeoPHPInterface {
+
+
   function __construct() {
     require_once(drupal_get_path('module', 'geophp') . '/geoPHP/geoPHP.inc');
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function version() {
+    return \geoPHP::version();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function load() {
+    return call_user_func_array(array('\geoPHP', 'load'), func_get_args());
+  }
+
 }
